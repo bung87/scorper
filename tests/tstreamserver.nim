0d9ab0cfccf1035b1bc165bd03a75590b1fa6083
bung87 <crc32@qq.com>
2020-11-23 15:59:21 +0800

    fix test

--- tests/tstreamserver.nim
+++ tests/tstreamserver.nim
@@ -10,15 +10,14 @@ from net import TimeoutError
 include ./looper/http/streamserver
 include ./looper/http/streamclient
 
-const TestUrl = "http://127.0.0.1:64123/foo?bar=qux"
+const TestUrl = "http://127.0.0.1:64124/foo?bar=qux"
 
 proc runTest(
-    handler: proc (request: Request): Future[void] {.closure, gcsafe.},
+    handler: proc (request: Request): Future[void] {.gcsafe.},
     request: proc (server: Looper): Future[AsyncResponse],
-    test: proc (response: AsyncResponse, body: string): Future[void]) {.async.} =
+    test: proc (response: AsyncResponse, body: string): Future[void])  =
 
-  let address = "127.0.0.1:64123"
-  let a = initTAddress(address)
+  let address = "127.0.0.1:64124"
   let flags = {ReuseAddr}
   var server = newLooper(address, handler, flags)
   server.start()
@@ -29,6 +28,7 @@ proc runTest(
   waitFor test(response, body)
   server.stop()
   server.close()
+  waitFor server.join()
 
 proc test200() {.async.} =
   proc handler(request: Request) {.async.} =
@@ -48,7 +48,7 @@ proc test200() {.async.} =
     doAssert(response.headers.hasKey("Content-Length"))
     doAssert(response.headers["Content-Length"] == "16")
 
-  await runTest(handler, request, test)
+  runTest(handler, request, test)
 
 proc test404() {.async.} =
   proc handler(request: Request) {.async.} =
@@ -59,7 +59,6 @@ proc test404() {.async.} =
       client = newAsyncHttpClient()
       clientResponse = await client.request(TestUrl)
     client.close()
-    server.close()
 
     return clientResponse
 
@@ -68,8 +67,8 @@ proc test404() {.async.} =
     doAssert(body == "Hello World, 404")
     doAssert(response.headers.hasKey("Content-Length"))
     doAssert(response.headers["Content-Length"] == "16")
-
-  await runTest(handler, request, test)
+  
+  runTest(handler, request, test)
 
 proc testCustomEmptyHeaders() {.async.} =
   proc handler(request: Request) {.async.} =
@@ -80,7 +79,6 @@ proc testCustomEmptyHeaders() {.async.} =
       client = newAsyncHttpClient()
       clientResponse = await client.request(TestUrl)
     client.close()
-    server.close()
 
     return clientResponse
 
@@ -90,7 +88,7 @@ proc testCustomEmptyHeaders() {.async.} =
     doAssert(response.headers.hasKey("Content-Length"))
     doAssert(response.headers["Content-Length"] == "16")
 
-  await runTest(handler, request, test)
+  runTest(handler, request, test)
 
 proc testCustomContentLength() {.async.} =
   proc handler(request: Request) {.async.} =
@@ -103,7 +101,6 @@ proc testCustomContentLength() {.async.} =
       client = newAsyncHttpClient()
       clientResponse = await client.request(TestUrl)
     client.close()
-    server.close()
 
     return clientResponse
 
@@ -113,7 +110,7 @@ proc testCustomContentLength() {.async.} =
     doAssert(response.headers.hasKey("Content-Length"))
     doAssert(response.headers["Content-Length"] == "0")
 
-  await runTest(handler, request, test)
+  runTest(handler, request, test)
 
 proc testPost() {.async.} =
   proc handler(request: Request) {.async.} =
@@ -125,14 +122,13 @@ proc testPost() {.async.} =
       client = newAsyncHttpClient()
       clientResponse = await client.post(TestUrl, body = "hello")
     client.close()
-    server.close()
     return clientResponse
 
   proc test(response: AsyncResponse, body: string) {.async.} =
     doAssert(response.status == Http200)
     doAssert(body == "Hello World, 200")
 
-  await runTest(handler, request, test)
+  runTest(handler, request, test)
 
 waitfor(test200())
 waitfor(test404())
