7d10b44f10c2f75e3a9eafac42f06330ad1fd592
bung87 <crc32@qq.com>
2020-11-23 21:53:45 +0800

    seperate response httpform from streamclient

---
+++ src/looper/http/asyncresponse.nim
@@ -0,0 +1,53 @@
+import httpcore, strutils
+import ./futurestream
+import times
+import chronos
+
+type
+  AsyncResponse* = ref object
+    version*: string
+    status*: string
+    headers*: HttpHeaders
+    body: string
+    bodyStream*: FutureStream[string]
+
+proc code*(response: AsyncResponse): HttpCode
+           {.raises: [ValueError, OverflowDefect].} =
+  ## Retrieves the specified response's ``HttpCode``.
+  ##
+  ## Raises a ``ValueError`` if the response's ``status`` does not have a
+  ## corresponding ``HttpCode``.
+  return response.status[0 .. 2].parseInt.HttpCode
+
+proc contentType*(response: AsyncResponse): string {.inline.} =
+  ## Retrieves the specified response's content type.
+  ##
+  ## This is effectively the value of the "Content-Type" header.
+  response.headers.getOrDefault("content-type")
+
+proc contentLength*(response: AsyncResponse): int =
+  ## Retrieves the specified response's content length.
+  ##
+  ## This is effectively the value of the "Content-Length" header.
+  ##
+  ## A ``ValueError`` exception will be raised if the value is not an integer.
+  var contentLengthHeader = response.headers.getOrDefault("Content-Length")
+  result = contentLengthHeader.parseInt()
+  doAssert(result >= 0 and result <= high(int32))
+
+proc lastModified*(response: AsyncResponse): DateTime =
+  ## Retrieves the specified response's last modified time.
+  ##
+  ## This is effectively the value of the "Last-Modified" header.
+  ##
+  ## Raises a ``ValueError`` if the parsing fails or the value is not a correctly
+  ## formatted time.
+  var lastModifiedHeader = response.headers.getOrDefault("last-modified")
+  result = parse(lastModifiedHeader, "ddd, dd MMM yyyy HH:mm:ss 'GMT'", utc())
+
+proc readBody*(response: AsyncResponse): Future[string] {.async.} =
+  ## Reads the response's body and caches it. The read is performed only
+  ## once.
+  if response.body.len == 0:
+    response.body = await readAll(response.bodyStream)
+  return response.body
--- src/looper/http/streamclient.nim
+++ src/looper/http/streamclient.nim
@@ -1,84 +1,16 @@
-import net, strutils, urlly, parseutils, base64, os, mimetypes, streams,
-  math, random, httpcore, times, tables, streams, std/monotimes
-import asyncnet, chronos, ./futurestream #, asyncfile
+import net, strutils, urlly, parseutils, base64, os, streams,
+  math,  httpcore, times, tables, streams, std/monotimes
+import asyncnet, chronos, ./futurestream , asyncresponse , httpform
 import nativesockets
-
+export asyncresponse
+export httpform
 export httpcore except parseHeader # TODO: The ``except`` doesn't work
-
-type
-  AsyncResponse* = ref object
-    version*: string
-    status*: string
-    headers*: HttpHeaders
-    body: string
-    bodyStream*: FutureStream[string]
-
-proc code*(response: AsyncResponse): HttpCode
-           {.raises: [ValueError, OverflowDefect].} =
-  ## Retrieves the specified response's ``HttpCode``.
-  ##
-  ## Raises a ``ValueError`` if the response's ``status`` does not have a
-  ## corresponding ``HttpCode``.
-  return response.status[0 .. 2].parseInt.HttpCode
-
-proc contentType*(response: AsyncResponse): string {.inline.} =
-  ## Retrieves the specified response's content type.
-  ##
-  ## This is effectively the value of the "Content-Type" header.
-  response.headers.getOrDefault("content-type")
-
-proc contentLength*(response: AsyncResponse): int =
-  ## Retrieves the specified response's content length.
-  ##
-  ## This is effectively the value of the "Content-Length" header.
-  ##
-  ## A ``ValueError`` exception will be raised if the value is not an integer.
-  var contentLengthHeader = response.headers.getOrDefault("Content-Length")
-  result = contentLengthHeader.parseInt()
-  doAssert(result >= 0 and result <= high(int32))
-
-proc lastModified*(response: AsyncResponse): DateTime =
-  ## Retrieves the specified response's last modified time.
-  ##
-  ## This is effectively the value of the "Last-Modified" header.
-  ##
-  ## Raises a ``ValueError`` if the parsing fails or the value is not a correctly
-  ## formatted time.
-  var lastModifiedHeader = response.headers.getOrDefault("last-modified")
-  result = parse(lastModifiedHeader, "ddd, dd MMM yyyy HH:mm:ss 'GMT'", utc())
-
-proc readBody*(response: AsyncResponse): Future[string] {.async.} =
-  ## Reads the response's body and caches it. The read is performed only
-  ## once.
-  if response.body.len == 0:
-    response.body = await readAll(response.bodyStream)
-  return response.body
 
 type
   Proxy* = ref object
     url*: Url
     auth*: string
 
-  MultipartEntry = object
-    name, content: string
-    case isFile: bool
-    of true:
-      filename, contentType: string
-      fileSize: int64
-      isStream: bool
-    else: discard
-
-  MultipartEntries* = openArray[tuple[name, content: string]]
-  MultipartData* = ref object
-    entries: seq[MultipartEntry]
-
-  ProtocolError* = object of IOError ## exception that is raised when server
-                                     ## does not conform to the implemented
-                                     ## protocol
-
-  HttpRequestError* = object of IOError ## Thrown in the ``getContent`` proc
-                                        ## and ``postContent`` proc,
-                                        ## when the server returns an error
 
 const defUserAgent* = "Nim httpclient/" & NimVersion
 
@@ -110,117 +42,6 @@ proc newProxy*(url: string, auth = ""): Proxy =
   ## Constructs a new ``TProxy`` object.
   result = Proxy(url: parseUrl(url), auth: auth)
 
-proc newMultipartData*: MultipartData {.inline.} =
-  ## Constructs a new ``MultipartData`` object.
-  MultipartData()
-
-proc `$`*(data: MultipartData): string =
-  ## convert MultipartData to string so it's human readable when echo
-  ## see https://github.com/nim-lang/Nim/issues/11863
-  const sep = "-".repeat(30)
-  for pos, entry in data.entries:
-    result.add(sep & center($pos, 3) & sep)
-    result.add("\nname=\"" & entry.name & "\"")
-    if entry.isFile:
-      result.add("; filename=\"" & entry.filename & "\"\n")
-      result.add("Content-Type: " & entry.contentType)
-    result.add("\n\n" & entry.content & "\n")
-
-proc add*(p: MultipartData, name, content: string, filename: string = "",
-          contentType: string = "", useStream = true) =
-  ## Add a value to the multipart data.
-  ##
-  ## When ``useStream`` is ``false``, the file will be read into memory.
-  ##
-  ## Raises a ``ValueError`` exception if
-  ## ``name``, ``filename`` or ``contentType`` contain newline characters.
-  if {'\c', '\L'} in name:
-    raise newException(ValueError, "name contains a newline character")
-  if {'\c', '\L'} in filename:
-    raise newException(ValueError, "filename contains a newline character")
-  if {'\c', '\L'} in contentType:
-    raise newException(ValueError, "contentType contains a newline character")
-
-  var entry = MultipartEntry(
-    name: name,
-    content: content,
-    isFile: filename.len > 0
-  )
-
-  if entry.isFile:
-    entry.isStream = useStream
-    entry.filename = filename
-    entry.contentType = contentType
-
-  p.entries.add(entry)
-
-proc add*(p: MultipartData, xs: MultipartEntries): MultipartData
-         {.discardable.} =
-  ## Add a list of multipart entries to the multipart data ``p``. All values are
-  ## added without a filename and without a content type.
-  ##
-  ## .. code-block:: Nim
-  ##   data.add({"action": "login", "format": "json"})
-  for name, content in xs.items:
-    p.add(name, content)
-  result = p
-
-proc newMultipartData*(xs: MultipartEntries): MultipartData =
-  ## Create a new multipart data object and fill it with the entries ``xs``
-  ## directly.
-  ##
-  ## .. code-block:: Nim
-  ##   var data = newMultipartData({"action": "login", "format": "json"})
-  result = MultipartData()
-  for entry in xs:
-    result.add(entry.name, entry.content)
-
-proc addFiles*(p: MultipartData, xs: openArray[tuple[name, file: string]],
-               mimeDb = newMimetypes(), useStream = true):
-               MultipartData {.discardable.} =
-  ## Add files to a multipart data object. The files will be streamed from disk
-  ## when the request is being made. When ``stream`` is ``false``, the files are
-  ## instead read into memory, but beware this is very memory ineffecient even
-  ## for small files. The MIME types will automatically be determined.
-  ## Raises an ``IOError`` if the file cannot be opened or reading fails. To
-  ## manually specify file content, filename and MIME type, use ``[]=`` instead.
-  ##
-  ## .. code-block:: Nim
-  ##   data.addFiles({"uploaded_file": "public/test.html"})
-  for name, file in xs.items:
-    var contentType: string
-    let (_, fName, ext) = splitFile(file)
-    if ext.len > 0:
-      contentType = mimeDb.getMimetype(ext[1..ext.high], "")
-    let content = if useStream: file else: readFile(file).string
-    p.add(name, content, fName & ext, contentType, useStream = useStream)
-  result = p
-
-proc `[]=`*(p: MultipartData, name, content: string) {.inline.} =
-  ## Add a multipart entry to the multipart data ``p``. The value is added
-  ## without a filename and without a content type.
-  ##
-  ## .. code-block:: Nim
-  ##   data["username"] = "NimUser"
-  p.add(name, content)
-
-proc `[]=`*(p: MultipartData, name: string,
-            file: tuple[name, contentType, content: string]) {.inline.} =
-  ## Add a file to the multipart data ``p``, specifying filename, contentType
-  ## and content manually.
-  ##
-  ## .. code-block:: Nim
-  ##   data["uploaded_file"] = ("test.html", "text/html",
-  ##     "<html><head></head><body><p>test</p></body></html>")
-  p.add(name, file.content, file.name, file.contentType, useStream = false)
-
-proc getBoundary(p: MultipartData): string =
-  if p == nil or p.entries.len == 0: return
-  while true:
-    result = $rand(int.high)
-    for i, entry in p.entries:
-      if result in entry.content: break
-      elif i == p.entries.high: return
 
 proc sendFile(socket: Socket | AsyncSocket,
               entry: MultipartEntry) {.async.} =
@@ -515,12 +336,7 @@ proc parseResponse(client: AsyncHttpClient,
   while not client.transp.atEof():
     linei = 0
     line = await client.transp.readLine()
-    # echo line
-    # if line == "":
-    #   # We've been disconnected.
-    #   client.close()
-    #   break
-    if line == httpNewLine or line == "":
+    if line == "":
       fullyRead = true
       break
     if not parsedStatus:
@@ -643,14 +459,7 @@ proc readFileSizes(client: AsyncHttpClient,
     let fileSize = getFileSize(entry.content)
     entry.fileSize = fileSize
 
-proc format(entry: MultipartEntry, boundary: string): string =
-  result = "--" & boundary & httpNewLine
-  result.add("Content-Disposition: form-data; name=\"" & entry.name & "\"")
-  if entry.isFile:
-    result.add("; filename=\"" & entry.filename & "\"" & httpNewLine)
-    result.add("Content-Type: " & entry.contentType & httpNewLine)
-  else:
-    result.add(httpNewLine & httpNewLine & entry.content)
+
 
 proc format(client: AsyncHttpClient,
             multipart: MultipartData): Future[seq[string]] {.async.} =
