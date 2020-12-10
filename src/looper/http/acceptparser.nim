25c673818bd9a78591b0ca0855d092245e4e7b28
bung87 <crc32@qq.com>
2020-12-11 02:32:27 +0800

    o

--- src/looper/http/acceptparser.nim
+++ src/looper/http/acceptparser.nim
@@ -4,14 +4,41 @@ export npeg
 
 proc sortFunc(x, y: tuple[mime:string,q:float]):int =
   result = cmp(x.q,y.q)
+  if result == 0:
+    let xoffset = x.mime.find('/')
+    let yoffset = y.mime.find('/')
+    if xoffset == yoffset and x.mime[0 ..< xoffset] == y.mime[0 ..< yoffset]:
+      let xsub = x.mime[xoffset+1 .. ^1]
+      let ysub = y.mime[yoffset+1 .. ^1]
+      if xsub == ysub:
+        return 0
+      elif ysub == "*":
+        return 1
+      elif xsub == "*":
+        return -1
 
 proc accpetParser*():Parser[char, seq[tuple[mime: string, q: float]]] = 
+  # https://tools.ietf.org/html/rfc7231#section-5.3.2
   result = peg("pairs",d:  seq[tuple[mime:string,q:float]]):
     pairs <- pair * *(',' * * Blank * pair) * eof
-    mime <- +(Alpha | Digit | '*' | '!' | '#' |
-                                '$' | '&' | '-' | '^' | '_' | '/' |  '+')
-    q <- +(Digit | '.')
-    pair <- >mime * ?(';' * ('q' | 'Q') * '=' * >q):
+    tchar <- Alpha | Digit | '*' | '!' | '#' |
+                                '$' | '&' | '-' | '^' | '_' | '/' |  '+'
+    token <- +tchar
+    DQUOTE <- '"'
+    # https://tools.ietf.org/html/rfc5234#appendix-B.1
+    VCHAR <- {'\x21' .. '\x7e'}
+    quoted_pair <- '\\' * ( Blank | VCHAR | obs_text )
+    quoted_string <- DQUOTE * ( qdtext | quoted_pair ) * DQUOTE
+    obs_text <- {'\x80' .. '\xff'}
+    qdtext <- Blank | '\x21' | {'\x21' .. '\x5b'} | {'\x5d' .. '\x7e'} | obs_text
+    
+    quoted_string <- Blank
+    parameter <- token * '=' * ( token | quoted_string )
+    mime <- token
+    q <- +( ('0' * ?('.' *  Digit[0..3])) | '1' * ?('.' * '0'[0..3]))
+    OWS <- *Blank
+    qkey <- 'q' | 'Q'
+    pair <- >mime * ?(';' * ((qkey * '=' * >q) | parameter) ):
       if capture.len == 3:
         let q = parseFloat $2
         if q > 0:
--- tests/tacceptparser.nim
+++ tests/tacceptparser.nim
@@ -1,45 +1,47 @@
 import ./looper/http/acceptparser
-
+import strutils
 block basic:
+  echo "text/html"[0 ..< "text/html".find('/')]
   let parser = accpetParser()
-  var mimes = newSeq[tuple[mime:string,q:float]]()
+  var mimes = newSeq[tuple[mime: string, q: float]]()
   let accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-  let r = parser.match(accept,mimes)
-  doAssert mimes == @[(mime: "text/html", q: 1.0), (mime: "application/xhtml+xml", q: 1.0), (mime: "application/xml", q: 0.9), (mime: "*/*", q: 0.8)]
+  let r = parser.match(accept, mimes)
+  doAssert mimes == @[(mime: "text/html", q: 1.0), (mime: "application/xhtml+xml", q: 1.0), (mime: "application/xml",
+      q: 0.9), (mime: "*/*", q: 0.8)]
 
 block defaultQuality:
   let parser = accpetParser()
   let accept = "application/json;q=0.2, text/html"
-  var mimes = newSeq[tuple[mime:string,q:float]]()
-  let r = parser.match(accept,mimes)
+  var mimes = newSeq[tuple[mime: string, q: float]]()
+  let r = parser.match(accept, mimes)
   doAssert mimes == @[(mime: "text/html", q: 1.0), (mime: "application/json", q: 0.2)]
 
 block order:
   let parser = accpetParser()
   let accept = "application/json;q=0.6, text/plain;q=0.8"
-  var mimes = newSeq[tuple[mime:string,q:float]]()
-  var r = parser.match(accept,mimes)
+  var mimes = newSeq[tuple[mime: string, q: float]]()
+  var r = parser.match(accept, mimes)
   doAssert mimes == @[(mime: "text/plain", q: 0.8), (mime: "application/json", q: 0.6)]
 
 block customParams:
   let parser = accpetParser()
   let accept = "text/*, text/plain;format=flowed, text/plain, text/plain;level=1, text/html, text/plain;level=2, */*, image/*, text/rich"
-  var mimes = newSeq[tuple[mime:string,q:float]]()
-  var r = parser.match(accept,mimes)
+  var mimes = newSeq[tuple[mime: string, q: float]]()
+  var r = parser.match(accept, mimes)
   echo mimes
 
 block disallows:
   let parser = accpetParser()
   let accept = "text/plain, application/json;q=0.5, text/html, text/drop;q=0"
-  var mimes = newSeq[tuple[mime:string,q:float]]()
-  var r = parser.match(accept,mimes)
+  var mimes = newSeq[tuple[mime: string, q: float]]()
+  var r = parser.match(accept, mimes)
   doAssert mimes == @[(mime: "text/plain", q: 1.0), (mime: "text/html", q: 1.0), (mime: "application/json", q: 0.5)]
 
 block mixed:
   let parser = accpetParser()
   let accept = "text/*, text/plain;format=flowed, text/plain, text/plain;level=1, text/html, text/plain;level=2, */*, image/*, text/rich"
-  var mimes = newSeq[tuple[mime:string,q:float]]()
-  var r = parser.match(accept,mimes)
+  var mimes = newSeq[tuple[mime: string, q: float]]()
+  var r = parser.match(accept, mimes)
   echo mimes
   #           'text/html',
   #           'text/plain;format=flowed',
