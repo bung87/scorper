ebfa1a1330d12587f28d6c1293b4ecbb261b68b8
bung87 <crc32@qq.com>
2020-12-19 15:58:20 +0800

    format

--- tests/tstreamserver.nim
+++ tests/tstreamserver.nim
@@ -15,7 +15,7 @@ const TestUrl = "http://127.0.0.1:64124/foo?bar=qux"
 proc runTest(
     handler: proc (request: Request): Future[void] {.gcsafe.},
     request: proc (server: Looper): Future[AsyncResponse],
-    test: proc (response: AsyncResponse, body: string): Future[void])  =
+    test: proc (response: AsyncResponse, body: string): Future[void]) =
 
   let address = "127.0.0.1:64124"
   let flags = {ReuseAddr}
@@ -52,7 +52,7 @@ proc test200() {.async.} =
 
 proc test404() {.async.} =
   proc handler(request: Request) {.async.} =
-    await request.resp( "Hello World, 404",code = Http404)
+    await request.resp("Hello World, 404", code = Http404)
 
   proc request(server: Looper): Future[AsyncResponse] {.async.} =
     let
@@ -67,12 +67,12 @@ proc test404() {.async.} =
     doAssert(body == "Hello World, 404")
     doAssert(response.headers.hasKey("Content-Length"))
     doAssert(response.headers["Content-Length"] == "16")
-  
+
   runTest(handler, request, test)
 
 proc testCustomEmptyHeaders() {.async.} =
   proc handler(request: Request) {.async.} =
-    await request.resp( "Hello World, 200", newHttpHeaders())
+    await request.resp("Hello World, 200", newHttpHeaders())
 
   proc request(server: Looper): Future[AsyncResponse] {.async.} =
     let
@@ -94,7 +94,7 @@ proc testCustomContentLength() {.async.} =
   proc handler(request: Request) {.async.} =
     let headers = newHttpHeaders()
     headers["Content-Length"] = "0"
-    await request.resp( "Hello World, 200", headers)
+    await request.resp("Hello World, 200", headers)
 
   proc request(server: Looper): Future[AsyncResponse] {.async.} =
     let
