269c83241db4ef849fefad630c60e592b81cee28
bung87 <crc32@qq.com>
2020-12-21 03:49:34 +0800

    o

--- benchmark/benchmark.nim
+++ benchmark/benchmark.nim
@@ -3,6 +3,9 @@ import os
 import strformat
 import locks
 import strutils
+import cpuinfo,math
+
+let threadsNum = nextPowerOfTwo(cpuinfo.countProcessors())
 
 const port {.intdefine.} = 8888
 const demoPath{.strdefine.} = "examples" / "hello_world_with_router.nim"
@@ -22,25 +25,25 @@ proc proj(){.thread.} =
   let project = startProcess(fmt"{dir / path}", options = {poEvalCommand})
   pid = project.processID
   workerChan.send(true)
-  var total:int
+  var total: int
   while project.running:
     let tried = projChan.tryRecv()
     if tried.dataAvailable:
-      inc total 
+      inc total
     if total == 2:
       project.terminate
       break
 
 proc root(){.thread.} =
   acquire(L)
-  let test = startProcess(fmt"wrk -t12 -c100 -d30s http://127.0.0.1:{port}/", options = testOptions)
+  let test = startProcess(fmt"wrk -t{threadsNum} -c100 -d30s http://127.0.0.1:{port}/", options = testOptions)
   let test1Code = waitForExit(test)
   projChan.send(1)
   release(L)
 
 proc pa(){.thread.} =
   acquire(L)
-  let test2 = startProcess(fmt"wrk -t12 -c100 -d30s http://127.0.0.1:{port}/p1/p2", options = testOptions)
+  let test2 = startProcess(fmt"wrk -t{threadsNum} -c100 -d30s http://127.0.0.1:{port}/p1/p2", options = testOptions)
   let test2Code = waitForExit(test2)
   projChan.send(2)
   release(L)
