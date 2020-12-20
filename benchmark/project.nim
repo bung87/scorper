2bc15c145cccf55c48281056d9b69afa61171c06
bung87 <crc32@qq.com>
2020-12-21 00:21:26 +0800

    add benchmark project

---
+++ benchmark/project.nim
@@ -0,0 +1,55 @@
+import osproc
+import os
+import strformat
+import times
+import locks
+import strutils
+
+const demoPath = currentSourcePath.parentDir / ".." / "examples" / "hello_world_with_router.nim"
+var
+  thr: array[3, Thread[void]]
+  L: Lock
+let n = 5000
+let c = 500
+let testOptions = {poEvalCommand,poParentStreams}
+var pid:int
+var projChan: Channel[int]
+projChan.open()
+var workerChan: Channel[bool]
+workerChan.open()
+proc proj(){.thread.} =
+  let (dir,path,ext) = demoPath.splitFile
+  let bin = startProcess(fmt"nim c -d:release {demoPath}",options={poEvalCommand})
+  discard waitForExit(bin)
+  let project = startProcess(fmt"{dir / path}",options={poEvalCommand})
+  pid = project.processID
+  workerChan.send(true)
+  while project.running:
+    let tried = projChan.tryRecv()
+    if tried.dataAvailable and tried.msg == 2:
+      project.terminate
+      break
+
+proc root(){.thread.} = 
+  acquire(L)
+  let test = startProcess(fmt"ab -v 1 -n {n} -c {c} -r http://127.0.0.1:8888/",options=testOptions)
+  let test1Code = waitForExit(test)
+  release(L)
+  projChan.send(1)
+
+proc pa(){.thread.} =
+  acquire(L)
+  let test2 = startProcess(fmt"ab -v 1 -n {n} -c {c} -r http://127.0.0.1:8888/p1/p2",options=testOptions)
+  let test2Code = waitForExit(test2)
+  release(L)
+  projChan.send(2)
+
+initLock(L)
+createThread(thr[0],proj)
+discard workerChan.recv() 
+createThread(thr[1],root)
+createThread(thr[2],pa)
+joinThreads(thr)
+deinitLock(L)
+workerChan.close
+projChan.close
--- examples/hello_world_with_router.nim
+++ examples/hello_world_with_router.nim
@@ -3,16 +3,11 @@ import looper
 when isMainModule:
   type AsyncCallback = proc (request: Request): Future[void] {.closure, gcsafe.}
   proc cb(req: Request) {.async.} =
-    echo req.hostname
-    echo req.meth
-    echo req.headers
-    echo req.protocol
-    echo req.url
-    echo req.params
     let headers = {"Content-type": "text/plain; charset=utf-8"}
     await req.resp("Hello World", headers.newHttpHeaders())
   
   let r = newRouter[AsyncCallback]()
+  r.addRoute(cb, "get","/")
   r.addRoute(cb, "get","/{p1}/{p2}")
   let address = "127.0.0.1:8888"
   let flags = {ReuseAddr}
