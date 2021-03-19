4e59b2854ab11ba859d5ec5dd816cd845c81fcab
bung87 <crc32@qq.com>
2021-03-20 07:48:15 +0800

    format code

--- src/looper/http/httpbasicauth.nim
+++ src/looper/http/httpbasicauth.nim
@@ -8,21 +8,21 @@ import sequtils
 import base64
 
 type HttpBasicAuthValidator* = ref object
-  validate:proc (request: Request, user,pass:string): Future[bool] {.closure, gcsafe.}
+  validate: proc (request: Request, user, pass: string): Future[bool] {.closure, gcsafe.}
 
-proc basicAuth*(request:Request, validator: HttpBasicAuthValidator): Future[bool] {.async.} =
+proc basicAuth*(request: Request, validator: HttpBasicAuthValidator): Future[bool] {.async.} =
   # https://tools.ietf.org/html/rfc2617#section-2
-  var success:bool
-  var up:seq[string]
+  var success: bool
+  var up: seq[string]
   try:
     let authorization = request.headers["Authorization"]
     let s2 = authorization.find("Basic")
     let decoded = base64.decode(authorization[s2+6 .. ^1])
-    up = decoded.split(":",1)
+    up = decoded.split(":", 1)
   except:
     await request.respError(Http400, "Authorization header not valid")
     return false
-  success = await validator.validate(up[0],up[1])
+  success = await validator.validate(up[0], up[1])
   if not success:
     await request.respBasicAuth()
     return false
@@ -32,4 +32,4 @@ when isMainModule:
   let s = "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=="
   let s2 = s.find("Basic")
   let decoded = base64.decode(s[s2+6 .. ^1])
-  let up = decoded.split(":",1)
+  let up = decoded.split(":", 1)
