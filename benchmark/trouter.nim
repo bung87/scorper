d1c1e617fcc52a4959c1d0b29ac5124fee6711fa
bung87 <crc32@qq.com>
2021-06-09 10:07:39 +0800

    o

--- benchmark/trouter.nim
+++ benchmark/trouter.nim
@@ -1,7 +1,9 @@
 import jester/patterns, tables
 import scorper/http/router
-
+import nest
 import times
+import std/httpcore
+import std/uri
 let t1 = cpuTime()
 for i in 1..1000:
   let f = parsePattern("/@p1/@p2")
@@ -9,17 +11,30 @@ for i in 1..1000:
   let f3 = parsePattern(r"/")
   doAssert(match(f3, r"/").matched)
 
-echo cpuTime() - t1
+echo "jester:", cpuTime() - t1
 
-proc testHandler() = echo "test"
+proc testHandler() = discard
+block:
+  let r = router.newRouter[proc()]()
+  r.addRoute(testHandler, "get", "/")
+  r.addRoute(testHandler, "get", "/{p1}/{p2}")
+  r.compress()
+  let t2 = cpuTime()
+  for i in 1..1000:
+    doAssert r.match("GET", "/").success == true
+    doAssert r.match("GET", "/p1/p2").success == true
 
-let r = newRouter[proc()]()
-r.addRoute(testHandler, "get", "/")
-r.addRoute(testHandler, "get", "/{p1}/{p2}")
-r.compress()
-let t2 = cpuTime()
-for i in 1..1000:
-  doAssert r.match("GET", "/").success == true
-  doAssert r.match("GET", "/p1/p2").success == true
+  echo "router:", cpuTime() - t2
 
-echo cpuTime() - t2
+block:
+  let r = nest.newRouter[proc()]()
+  r.map(testHandler, "get", "/")
+  r.map(testHandler, "get", "/{p1}/{p2}")
+  r.compress()
+  const r1 = parseUri"/"
+  const r2 = parseUri"/p1/p2"
+  let t2 = cpuTime()
+  for i in 1..1000:
+    doAssert r.route(HttpGet, r1).status == routingSuccess
+    doAssert r.route(HttpGet, r2).status == routingSuccess
+  echo "nest router:", cpuTime() - t2
