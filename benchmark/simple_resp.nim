f49aef85cca434a158d5343644a61d90581c1611
bung87 <crc32@qq.com>
2021-06-16 12:17:23 +0800

    o

--- benchmark/simple_resp.nim
+++ benchmark/simple_resp.nim
@@ -1,7 +1,7 @@
 import std / [exitprocs]
 import scorper
 import jsony
-
+from osproc import countProcessors
 const port {.intdefine.} = 8080
 
 type Resp = object
@@ -15,15 +15,25 @@ proc plaintextHandler(req: Request) {.route("get", "/plaintext"), async.} =
   let headers = {"Content-type": "text/plain"}
   await req.resp("Hello, World!", @headers)
 
-when isMainModule:
-
+proc threadFunc(){.thread.} =
   let address = "0.0.0.0:" & $port
-  let flags = {ReuseAddr}
+  let flags = {ReuseAddr, ReusePort}
   let r = newRouter[ScorperCallback]()
   r.addRoute(jsonHandler)
   r.addRoute(plaintextHandler)
   var server = newScorper(address, r, flags)
-  exitprocs.addExitProc proc() = server.stop(); waitFor server.closeWait()
+  onThreadDestruction proc(){.raises: [].} =
+    try:
+      server.stop(); waitFor server.closeWait()
+    except:
+      discard
   server.start()
-
   waitFor server.join()
+
+when isMainModule:
+  let numThreads = countProcessors()
+  var thr = newSeq[Thread[void]](numThreads)
+  for i in 0..high(thr):
+    # pinToCpu(thr[i],i+1)
+    createThread(thr[i], threadFunc)
+  joinThreads(thr)
