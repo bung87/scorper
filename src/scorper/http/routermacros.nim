da0694029211f09a471eef3f553228a39b8097bb
bung87 <crc32@qq.com>
2021-06-20 05:35:36 +0800

    o

--- src/scorper/http/routermacros.nim
+++ src/scorper/http/routermacros.nim
@@ -17,9 +17,30 @@ template addRoute*[H](
   else:
     {.error: "handler should has route pragma".}
 
+macro regRoute*(prc: untyped) =
+  result = newStmtList()
+  var n = prc[0]
+  let
+    router = genSym(nskVar, "router")
+    mount = ident("mount")
+  result.add quote do:
+    var `router`{.compileTime.}: seq[NimNode]
+
+  result.add quote do:
+    `prc`
+    `router`.add n
+
+  result.add quote do:
+    when not declared(`mount`):
+      macro `mount`*(r: untyped) =
+        result = newStmtList()
+        for sym in `router`:
+          result.add newCall("addRoute", r, sym)
+
 when isMainModule:
-  proc handler(req: Request) {.route("get", "/one"), async.} = discard
-  proc handler2(req: Request) {.route(["get", "post"], "/multi"), async.} = discard
+  proc handler(req: Request) {.route("get", "/one"), regRoute, async.} = discard
+  proc handler2(req: Request) {.route(["get", "post"], "/multi"), regRoute, async.} = discard
   let r = newRouter[ScorperCallback]()
-  r.addRoute(handler)
-  r.addRoute(handler2)
+  # r.addRoute(handler)
+  # r.addRoute(handler2)
+  r.mount()
