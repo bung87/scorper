ad4f8c250c803f11852e95839255c55cfe5440be
bung87 <crc32@qq.com>
2023-08-06 21:14:30 +0800

    fix all path

--- src/scorper.nim
+++ src/scorper.nim
@@ -1,10 +1,10 @@
-import scorper / http / streamserver
-import scorper / http / router
-import scorper / http / httprequest
-import scorper / http / httpcore
-import scorper / http / httpform
-import scorper / http / cookies
-import scorper / http / routermacros
+import scorper/http/streamserver
+import scorper/http/router
+import scorper/http/httprequest
+import scorper/http/httpcore
+import scorper/http/httpform
+import scorper/http/cookies
+import scorper/http/routermacros
 export router, streamserver, httpform, cookies
 import chronos
 export chronos
--- tests/mroutes.nim
+++ tests/mroutes.nim
@@ -1,4 +1,4 @@
 import chronos
-import ./scorper/http / [streamserver, httprequest, router, httpcore, routermacros]
+import scorper/http/[streamserver, httprequest, router, httpcore, routermacros]
 proc handler*(req: Request) {.route("get", "/one"), async.} = discard
 proc handler2*(req: Request) {.route(["get", "post"], "/multi"), async.} = discard
